4719fcc0ff53f550df16fc0be1df8229d4c965d1
Jao <jvplorencini@gmail.com>
2024-12-03 21:39:59 -0300

    Salvamento das informações dos eventos no banco de dados(falta criar os eventos em html)

--- assets/php/criaEvento.php
+++ assets/php/criaEvento.php
@@ -2,30 +2,30 @@
 require 'connect.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $titulo = $_POST['addTituloPrincipal'];
-    $subtitulo = $_POST['addSubtitulo'];
-    $preview = $_POST['addPreview'];
+    $nomeEvent = $_POST['addNomeEvent'];
+    $data = $_POST['addDataEvent'];
+    $locEvent = $_POST['addLocEvent'];
     $descricao = $_POST['addDescricaoEvent'];
 
-    if (isset($_FILES['addImagemNoticia']) && $_FILES['addImagemNoticia']['error'] === UPLOAD_ERR_OK) {
-        $diretorio_upload = '../images/uploads/';
-        $nome_imagem = uniqid() . '-' . basename($_FILES['addImagemNoticia']['name']);
+    if (isset($_FILES['addImgEvent']) && $_FILES['addImgEvent']['error'] === UPLOAD_ERR_OK) {
+        $diretorio_upload = '../images/uploads/eventos/';
+        $nome_imagem = uniqid() . '-' . basename($_FILES['addImgEvent']['name']);
         $caminho_imagem = $diretorio_upload . $nome_imagem;
         if (!is_dir($diretorio_upload)) {
             mkdir($diretorio_upload, 0777, true);
         }
-        if (move_uploaded_file($_FILES['addImagemNoticia']['tmp_name'], $caminho_imagem)) {
-            $sql = 'INSERT INTO eventos (titulo, subtitulo, imagem, preview, descricao) VALUES (?, ?, ?, ?, ?)';
+        if (move_uploaded_file($_FILES['addImgEvent']['tmp_name'], $caminho_imagem)) {
+            $sql = 'INSERT INTO eventos (nomeEvent, data, imagem, locEvent, descricao) VALUES (?, ?, ?, ?, ?)';
             $stmt = $conn->prepare($sql);
             
             if ($stmt === false) {
                 die("Erro ao preparar a declaração: " . $conn->error);
             }
-            $stmt->bind_param("sssss", $titulo, $subtitulo, $nome_imagem, $preview, $descricao);
+            $stmt->bind_param("sssss", $nomeEvent, $data, $nome_imagem, $locEvent, $descricao);
             if ($stmt->execute()) {
-                echo "Noticia Adicionada com sucesso!";
+                echo "Evento Adicionado com sucesso!";
             } else { 
-                echo "Erro ao adicionar a notícia. Código: " . $stmt->error; 
+                echo "Erro ao adicionar o Evento. Código: " . $stmt->error; 
             }
             $stmt->close();
         } else {
